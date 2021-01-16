25f917d54df1e02f86d7e61a6c9883d51c472be1
Stephen Algeo <stephen.algeo@williamhill.com>
2021-01-16 20:12:26 +0000

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
+// @ts-nocheck
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [items, setItems] = React.useState([])
+  const [postcode, setPostcode] = React.useState('')
+  const [search, setSearch] = React.useState('')
+  const [page, setPage] = React.useState(0)
+  
+  React.useEffect(() => {
+    const doFetch = async () => {
+      const path = `https://landregistry.data.gov.uk/data/ppi/transaction-record.json?_page=${page}&propertyAddress.postcode=${encodeURI(search.toUpperCase())}&_sort=-transactionDate`;
+                 // https://landregistry.data.gov.uk/data/ppi/transaction-record.json?_page=0&propertyAddress.postcode=BD13%202SL
+      const response = await fetch(path);
+      const data = await response.json();
+      setItems(data.result.items);
+    }
+
+    search !== '' && doFetch();
+  }, [search, page]);
+
+  console.warn(`Searching for ${search}, Page ${page}, length: ${items.length}`)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Fake Mouse Price
       </header>
+      <div>
+         <input value={postcode} onChange={(e) => setPostcode(e.target.value)} onKeyPress={(e)=> {
+           e.code === 'Enter' && setSearch(postcode)
+         }} />
+         <div>{search !== '' && items.length === 0 && <span>No more results</span>}</div>
+        <table>
+          <tbody>
+            {items.map((property,index) => (
+              <tr key={index}>
+                <td>{property.propertyAddress.saon} {property.propertyAddress.paon}</td>
+                <td>{property.propertyAddress.street}, {property.propertyAddress.town}</td>
+                <td>£{property.pricePaid}</td>
+                <td>{property.transactionDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={() => setPage(page-1)} disabled={page === 0}>Prev</button>
+        <button onClick={() => setPage(page+1)} disabled={items.length === 0}>Next</button>
+      </div>
     </div>
   );
 }
